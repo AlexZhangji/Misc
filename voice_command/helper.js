15c44f9d61065bebffc25655331381477f7ae1ea
zhang ji <zhan586@usc.edu>
2016-04-10 18:59:42 -0700

    working voice input

--- voice_command/helper.js
+++ voice_command/helper.js
@@ -1,24 +1,20 @@
 $(document).ready(function() {
 
   $('#btn1').click(function() {
-    add_Cards();
+    // add_Cards();
     var input = $('#search_img').val();
     // var input = './img/disk.jpg';
     search_img(input);
 
     // reload page
-    $('#inner').isotope('reloadItems').isotope({
-      sortBy: 'original-order'
-    });
+    // $('#inner').isotope('reloadItems').isotope({
+    //   sortBy: 'original-order'
+    // });
   });
 
   // disk appear
   $('#btn2').click(function() {
-    $('#wild_img').animate({
-      // bottom: '00px'
-      top: '50px',
-      right: '100px',
-    }, 1000);
+      album_appear();
   });
 
   // restore
@@ -27,68 +23,70 @@ $(document).ready(function() {
   });
 
 
-  function search_img(term) {
-    var params = {
-      // Request parameters
-      "q": term,
-      "count": "10",
-      "offset": "0",
-      "mkt": "en-us",
-      "safeSearch": "Moderate",
-    };
-
-    $.ajax({
-        url: "https://bingapis.azure-api.net/api/v5/images/search?" + $.param(params),
-        beforeSend: function(xhrObj) {
-          // Request headers
-          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "4abb72b2eea943fa8de128c9ee46d07d");
-        },
-        type: "GET",
-        // Request body
-        data: "{body}",
-      })
-      .done(function(data) {
-        // alert("success");
-        console.log(data);
-        var img_url = data.value[0].contentUrl;
-        // $('#img_api').attr("src", img_url);
-        console.log('img_url:' + img_url);
-        // return img_url;
-        add_Cards(img_url, 'img');
-
-      })
-      .fail(function() {
-        // alert("error");
-        console.log('error');
-        return 'failed';
-      });
-  }
 
 });
 
+function search_img(term) {
+  var params = {
+    // Request parameters
+    "q": term,
+    "count": "10",
+    "offset": "0",
+    "mkt": "en-us",
+    "safeSearch": "Moderate",
+  };
+
+  $.ajax({
+      url: "https://bingapis.azure-api.net/api/v5/images/search?" + $.param(params),
+      beforeSend: function(xhrObj) {
+        // Request headers
+        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "4abb72b2eea943fa8de128c9ee46d07d");
+      },
+      type: "GET",
+      // Request body
+      data: "{body}",
+    })
+    .done(function(data) {
+      // alert("success");
+      console.log(data);
+      var img_url = data.value[0].contentUrl;
+      // $('#img_api').attr("src", img_url);
+      console.log('img_url:' + img_url);
+      // return img_url;
+      add_Cards(img_url, 'img');
+
+    })
+    .fail(function() {
+      // alert("error");
+      console.log('error');
+      return 'failed';
+    });
+}
+
+
 // voice helper
-  function fall() {
-    console.log('fall');
-    $("#inner").sortable('disable');
-    $("#inner").css({
-      'top': '0px',
-      'left': '0px',
-      'width': 'initial',
-      'height': 'initial'
-    });
-    runs = "start";
-    $('#inner').jGravity({
-      target: 'div.gravity',
-      ignoreClass: 'div.steady',
-      weight: 10,
-      depth: 5,
-      drag: true
-    });
-    $(this).hide(300);
-    $(".steady").animate({
-      left: '0px'
-    }, 300);
-  }
+function fall() {
+  console.log('fall');
+  $("#inner").sortable('disable');
+  $("#inner").css({
+    'top': '0px',
+    'left': '0px',
+    'width': 'initial',
+    'height': 'initial'
+  });
+  runs = "start";
+  $('#inner').jGravity({
+    target: 'div.gravity',
+    ignoreClass: 'div.steady',
+    weight: 10,
+    depth: 5,
+    drag: true
+  });
+  $(this).hide(300);
+  $(".steady").animate({
+    left: '0px'
+  }, 300);
+}
 
 function nono() {
   alert('Nono, bad! bad !');
@@ -113,9 +111,29 @@ function restore() {
   });
 }
 
+function album_appear() {
+  console.log('album appear')
+  $('#wild_img').animate({
+    // bottom: '00px'
+    top: '50px',
+    right: '100px',
+  }, 1000);
+}
 
 function showImages(ss) {
-  alert(ss);
+
+  // alert(ss);
+  if (ss.indexOf('cat') !== -1) {
+    $('#cat_img').animate({
+      bottom: '-30px'
+    });
+    console.log('voice:' + ss);
+    console.log('cat appear!');
+  } else {
+    console.log('voice_2:' + ss);
+    // alert('show me ' + ss);
+    search_img(ss);
+  }
 }
 
 
@@ -130,5 +148,8 @@ function add_Cards(input, type) {
   }
   // sort after add
   // $('#inner').isotope('reLayout');
+  $('#inner').isotope('reloadItems').isotope({
+    sortBy: 'original-order'
+  });
 
 }
